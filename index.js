82f41f3e4cedc7ba03579803dc524b9e1a46c285
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-10-05 12:48:52 -0400

    edited index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const generatePage = require('./src/page-template');
+const writeFile = require('./src/generate-site')
+
+let manager = [];
+let enginner = [];
+let intern = [];
+let emplyoeeArr = {manager, engineer, intern};
+
